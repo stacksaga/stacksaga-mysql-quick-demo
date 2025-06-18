4b30108799184dbe74f8e45f363b46bf34a63f60
mafei <mafei.dev@gmail.com>
2025-06-18 20:58:41 +0530

    seperated common components in one lib and everything work well for both async starter and sync starter

--- src/main/java/demo/quickdemo/StacksagaQuickDemoApplication.java
+++ src/main/java/demo/quickdemo/StacksagaQuickDemoApplication.java
@@ -1,33 +1,24 @@
 package demo.quickdemo;
 
-import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.stacksaga.Aggregator;
-import org.stacksaga.autoconfigure.property.StacksagaProperties;
-import org.stacksaga.core.DefaultKeyGen;
-import org.stacksaga.executor.SagaExecutor;
-import reactor.core.publisher.Hooks;
-
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import stacksaga.async.core.SagaEventManager;
 
 @SpringBootApplication(
 //        exclude = R2dbcAutoConfiguration.class
 )
-@Import({
-        StacksagaProperties.class,
-        DefaultKeyGen.class,
-})
-public class StacksagaQuickDemoApplication {
+public class StacksagaQuickDemoApplication implements CommandLineRunner {
+    private final ApplicationContext applicationContext;
+
+    public StacksagaQuickDemoApplication(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer(ApplicationContext applicationContext) {
         return new WebMvcConfigurer() {
@@ -44,5 +35,11 @@ public class StacksagaQuickDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StacksagaQuickDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+//        SagaEventManager bean = this.applicationContext.getBean(SagaEventManager.class);
+//        System.out.println("bean = " + bean);
     }
 }
